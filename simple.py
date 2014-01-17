9472fe328dd6de62f14e4e8f9ebe4b05e2f2dbfc
gerbal <grant.mclendon@unc.edu>
2014-01-17 20:32:30 +0000

    fixing port issue on heroku, hopefully

--- simple.py
+++ simple.py
@@ -371,4 +371,5 @@ if __name__ == "__main__":
             except IOError:
                 pass
 
-    app.run(host="0.0.0.0", port=4000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
